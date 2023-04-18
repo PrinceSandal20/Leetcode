7ea55a0a773622970b50981581caf3cef50d5e6b
Prince <84025662+PrinceSandal20@users.noreply.github.com>
2023-04-18 15:08:05 +0530

    Time: 17 ms (44.49%), Space: 17.9 MB (12.91%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int>st;
+        vector<int>ans;
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]>0 || st.empty()){
+                st.push(asteroids[i]);
+            }
+            else{
+                if(!st.empty() && st.top()>0  && st.top()<abs(asteroids[i])){
+                    while(!st.empty() &&  st.top()>0  && st.top()<abs(asteroids[i])){
+                        st.pop();
+                    }
+                }
+                if(!st.empty() && st.top()==abs(asteroids[i])){
+                    st.pop();
+                }
+                else if(st.empty() || st.top()<0){
+                    st.push(asteroids[i]);
+                }
+            }
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
