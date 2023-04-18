225d970dba78d10ce286c22f997469bf44289591
Prince <84025662+PrinceSandal20@users.noreply.github.com>
2023-04-18 14:40:47 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (19.57%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int i=0,j=0;
+        if(word1.size()==word2.size())
+        {
+            while(i<word1.size() && j<word1.size())
+            {
+                ans+=word1[i];
+                ans+=word2[j];
+                i++,j++;
+            }
+        }
+        else if(word1.size()<word2.size())
+        {
+            while(i<word1.size() || j<word2.size())
+            {
+                if(i<word1.size())
+                {
+                    ans+=word1[i];
+                }
+                ans+=word2[j];
+                i++,j++;
+            }
+        }
+        else if(word1.size()>word2.size())
+        {
+            while(i<word1.size() || j<word2.size())
+            {
+                ans+=word1[i];
+                if(j<word2.size())
+                {
+                    ans+=word2[j];
+                }
+                i++,j++;
+            }
+        }
+        return ans;
+    }
+};
