25be97001f942de6481e1a81d669bae23a33c499
Prince <84025662+PrinceSandal20@users.noreply.github.com>
2023-05-25 15:55:55 +0530

    Time: 299 ms (13.47%), Space: 70.8 MB (17.08%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -7,19 +7,18 @@ public:
             mp[edges[i][1]].push_back({edges[i][0],succProb[i]});
         }
         vector<double>prob(n,0.0);
-        priority_queue<pair<double,int>>q;
-        q.push({1.0,start});
         prob[start]=1.0;
+        queue<int>q;
+        q.push(start);
         while(!q.empty()){
-            double p=q.top().first;
-            int node=q.top().second;
+            int node=q.front();
             q.pop();
             for(auto val:mp[node]){
                 int newnode=val.first;
                 double res=val.second;
-                if(prob[newnode]<res*p){
-                    prob[newnode]=res*p;
-                    q.push({prob[newnode],newnode});
+                if(prob[newnode]<res*prob[node]){
+                    prob[newnode]=res*prob[node];
+                    q.push(newnode);
                 }
             }
         }
