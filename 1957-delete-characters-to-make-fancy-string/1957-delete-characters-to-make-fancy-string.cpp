b119d297e2c49ffc6cb99d922ff0954cd890a2ae
Prince <84025662+PrinceSandal20@users.noreply.github.com>
2023-05-27 01:25:03 +0530

    Time: 118 ms (84.56%), Space: 41.2 MB (29.82%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        stack<pair<int,int>>st;
-        for(int i=0;i<s.size();i++){
-            if(!st.empty() && st.top().first==s[i]){
-                st.push({s[i],st.top().second+1});
-                if(st.top().second==3){
-                    st.pop();
-                }
-            }
-            else{
-                st.push({s[i],1});
-            }
-        }
         string ans="";
-        while(!st.empty()){
-            ans+=st.top().first;
-            st.pop();
+        ans+=s[0];
+        for(int i=1;i<s.size();i++){
+            if(s[i-1]==s[i] && s[i]==s[i+1]){
+                continue;
+            }
+            ans+=s[i];
         }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
