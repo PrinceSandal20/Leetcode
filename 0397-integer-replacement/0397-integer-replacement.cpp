b14fb3a13ce424fb6ca995eac9863c8ddbe477e2
Prince <84025662+PrinceSandal20@users.noreply.github.com>
2023-06-02 01:55:50 +0530

    Time: 0 ms (100.00%), Space: 10 MB (16.76%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int dfs(long long int n){
+    int dfs(long long int n,unordered_map<long long int,long long int>&dp){
         if(n==1){
             return 0;
         }
+        if(dp.count(n)>0){
+            return dp[n];
+        }
         int a=INT_MAX,b=INT_MAX,c=INT_MAX;
         if(n%2==0){
-            a=1+dfs(n>>1);
+            a=1+dfs(n>>1,dp);
         }
         else{
-            b=1+dfs(n+1);
-            c=1+dfs(n-1);
+            b=1+dfs(n+1,dp);
+            c=1+dfs(n-1,dp);
         }
-        return min({a,b,c});
+        return dp[n]=min({a,b,c});
     }
     int integerReplacement(int n) {
-        return dfs(n);
+        unordered_map<long long int,long long int>dp;
+        return dfs(n,dp);
     }
 };
