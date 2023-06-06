61c3baa5ea4b877d408c7d269cac6e64045e1196
Prince <84025662+PrinceSandal20@users.noreply.github.com>
2023-06-06 13:16:01 +0530

    Time: 4 ms (74.54%), Space: 9.2 MB (28.46%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -6,7 +6,6 @@ public:
         for(int i=2;i<arr.size();i++){
             if(arr[i]-arr[i-1]!=diff){
                 return false;
-                break;
             }
         }
         return true;
