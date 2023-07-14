7396ad457bd6ef96d1d86feb5f56695cb5a9dabb
Prince <84025662+PrinceSandal20@users.noreply.github.com>
2023-07-14 18:44:24 +0530

    Time: 0 ms (100.00%), Space: 7 MB (48.67%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+       int n=s.size(),m=p.size();
+       vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+       dp[0][0]=true;
+       for(int j=1;j<=m;j++){
+           if(p[j-1]=='*'){
+               dp[0][j]=dp[0][j-2];
+           }
+       }
+       for(int i=1;i<=n;i++){
+           dp[i][0]=false;
+       }
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=m;j++){
+              if(p[j-1]==s[i-1] || p[j-1]=='.'){
+                  dp[i][j]=dp[i-1][j-1];
+              }
+              else if(p[j-1]=='*'){
+                  if(p[j-2]==s[i-1] || p[j-2]=='.'){
+                      dp[i][j]=dp[i-1][j] || dp[i][j-2];
+                  }
+                  else{
+                      dp[i][j]=dp[i][j-2];
+                  }
+              }
+              else{
+                  dp[i][j]=0;
+              }
+           }
+       }
+       return dp[n][m];
+    }
+};
